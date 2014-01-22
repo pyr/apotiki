7f2f29a14ffc0b755a1c6504a51ba025ffc97ca2
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-01-22 10:57:33 +0100

    make ar parser standalone

--- Apotiki/Ar.hs
+++ Apotiki/Ar.hs
@@ -1,13 +1,12 @@
 module Apotiki.Ar (arFromData, arFromFile, ArEntry (..)) where
-import Control.Exception.Base
-import Data.Attoparsec.Combinator
+import Data.Attoparsec.Combinator (manyTill)
+import Data.ByteString.Char8 (pack, unpack)
 import qualified Data.Map as M
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import qualified Data.Attoparsec.ByteString as P
 
-armag = "!<arch>\n"
-arfmag = "`\n"
+armag = "!<arch>\n" -- magic header
+arfmag = "`\n" -- magic pad
 
 data ArEntry = ArEntry { -- A file entry in an ar file
   entryDate :: Int,
@@ -19,42 +18,25 @@ data ArEntry = ArEntry { -- A file entry in an ar file
   } deriving Show
 
 type ArMapEntry = (String, ArEntry)
-
 type ArMap = M.Map String ArEntry
 
 arEntryParser :: P.Parser ArMapEntry
 arEntryParser = do
-  name_b <- P.take 16
-  let name = takeWhile (/= '/') $ BC.unpack name_b
-  date_b <- P.take 12
-  let date = read (BC.unpack date_b) :: Int
-  gid_b <- P.take 6
-  let gid = read (BC.unpack gid_b) :: Int
-  uid_b <- P.take 6
-  let uid = read (BC.unpack uid_b) :: Int
-  mode_b <- P.take 8
-  let mode = read (BC.unpack mode_b) :: Int
-  size_b <- P.take 10
-  let size = read (BC.unpack size_b) :: Int
-  magic <- P.string $ BC.pack arfmag
+  let not_space = (\c -> (c /= '/') && (c /= ' '))
+  name <- (takeWhile not_space . unpack) `fmap` P.take 16
+  date <- (read . unpack) `fmap` P.take 12
+  gid <- (read . unpack) `fmap` P.take 6
+  uid <- (read . unpack) `fmap` P.take 6
+  mode <- (read . unpack) `fmap` P.take 8
+  size <- (read . unpack) `fmap` P.take 10
+  magic <- P.string $ pack arfmag
   payload <- P.take size
-  padding <- if size `mod` 2 == 1 then
-               P.string $ BC.pack "\n"
-             else
-               P.take 0
-  return (name,
-          ArEntry {
-            entryDate = date,
-            entryGid = gid,
-            entryUid = uid,
-            entryMode = mode,
-            entrySize = size,
-            entryData = payload
-            })
+  padding <- if size `mod` 2 == 1 then P.string $ pack "\n" else P.take 0
+  return (name, ArEntry date gid uid mode size payload)
 
 arParser :: P.Parser ArMap
 arParser = do
-  magic <- P.string $ BC.pack armag
+  magic <- P.string $ pack armag
   entries <- manyTill arEntryParser P.endOfInput
   return (M.fromList entries)
 
@@ -65,8 +47,3 @@ arFromFile :: String -> IO (Either String ArMap)
 arFromFile path = do
   content <- B.readFile path
   return (arFromData content)
-
-test :: String -> IO ()
-test path = do
-  (Right ar_map) <- arFromFile path
-  putStrLn $ show $ ar_map M.! "debian-binary"
