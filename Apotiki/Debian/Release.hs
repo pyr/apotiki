3f5d35c88caf3cf7f50ac6551c42059ce25951eb
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-01-22 20:00:45 +0100

    add release production logic

--- Apotiki/Debian/Release.hs
+++ Apotiki/Debian/Release.hs
@@ -1,10 +1,29 @@
 module Apotiki.Debian.Release where
 import Apotiki.Debian.Package
 import Apotiki.Debian.Control
+import Apotiki.FileInfo
+import Apotiki.Config
+import Apotiki.Signature
 import System.Directory
 import Data.List
 import Data.Function
+import Data.ByteString.Char8 (pack,unpack)
+import qualified Codec.Compression.GZip as Z
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Map as M
+
+type ArchRelease = M.Map String DebInfo
+type Release = M.Map String ArchRelease
+
+flatinfo fileinfo = [fileinfo M.! "Size", fileinfo M.! "MD5sum",
+                     fileinfo M.! "SHA1", fileinfo M.! "SHA256"]
+
+unrollEntry (k,v) = k ++ ": " ++ v
+unrollMap input = concat $ intersperse "\n" $ map unrollEntry $ M.assocs input
+
+unroll :: [DebInfo] -> String
+unroll input = (concat $ intersperse "\n\n" $ map unrollMap input) ++ "\n"
 
 pkgControl pooldir arch pkg = do
   let path = pooldir ++ "/" ++ arch ++ "/" ++ pkg ++ "/control"
@@ -57,6 +76,114 @@ mergeRelease new old arch =
 updateRelease archs old new = M.fromList (zip archs updated) where
   updated = map (mergeRelease new old) archs
 
+getPkg :: ApotikiConfig -> (String, ArchRelease) -> (String, B.ByteString)
+getPkg config (arch, release) = (relpath, pack str_data) where
+  distdir = (configDistDir config)
+  component = (configComponent config)
+  relname = (configRelease config)
+  origin = (configOrigin config)
+  label = (configLabel config)
+  path = concat $ intersperse "/"
+         [distdir, relname, component,
+          ("binary-" ++ arch), "Packages"]
+  relpath = concat $ intersperse "/"
+            [component, ("binary-" ++ arch), "Packages"]
+  str_data = unroll $ map snd $ M.assocs release
 
-writeRelease path release = do
-  writeFile path $ show release
+writePackages :: ApotikiConfig -> (String, B.ByteString) -> IO (String, [String])
+writePackages config (relpath, payload) = do
+  let path = concat $ intersperse "/" [(configDistDir config),
+                                       (configRelease config),
+                                       relpath]
+  B.writeFile path payload
+  return (relpath, flatinfo $ fileinfo payload)
+
+writeGzPackages :: ApotikiConfig -> (String, B.ByteString) -> IO (String, [String])
+writeGzPackages config (relpath, payload) = do
+  let path = concat $ intersperse "/" [(configDistDir config),
+                                       (configRelease config),
+                                       relpath ++ ".gz"]
+  let gzpayload = B.concat $ BL.toChunks $ Z.compress $ BL.fromChunks [payload]
+  B.writeFile path gzpayload
+  return (relpath ++ ".gz", flatinfo $ fileinfo gzpayload)
+
+writeArchRelease :: ApotikiConfig -> (String, ArchRelease) -> IO (String, [String])
+writeArchRelease config (arch,release) = do
+  let distdir = (configDistDir config)
+  let component = (configComponent config)
+  let relname = (configRelease config)
+  let origin = (configOrigin config)
+  let label = (configLabel config)
+  let path = concat $ intersperse "/"
+             [distdir, relname, component,
+              ("binary-" ++ arch), "Release"]
+  let relpath = concat $ intersperse "/"
+                [component, ("binary-" ++ arch), "Release"]
+
+  let payload = pack $ unroll [M.fromList [("Archive", relname),
+                                           ("Component", component),
+                                           ("Origin", origin),
+                                           ("Label", label),
+                                           ("Architecture", arch)]]
+  B.writeFile path payload
+  return (relpath, flatinfo $ fileinfo payload)
+
+md5info (path, [size, sum, _, _]) = " " ++ sum ++ " " ++ size ++ " " ++ path
+sha1info (path, [size, _, sum, _]) = " " ++ sum ++ " " ++ size ++ " " ++ path
+sha256info (path, [size, _, _, sum]) = " " ++ sum ++ " " ++ size ++ " " ++ path
+
+writeGlobalRelease :: ApotikiConfig -> [(String, [String])] -> IO ()
+writeGlobalRelease config info = do
+  let archs = concat $ intersperse " " (configArchs config)
+  let origin = configOrigin config
+  let label = configLabel config
+  let release = configRelease config
+  let component = configComponent config
+  let md5s = concat $ intersperse "\n" $ map md5info info
+  let sha1s = concat $ intersperse "\n" $ map sha1info info
+  let sha256s = concat $ intersperse "\n" $ map sha256info info
+  let sums = concat $ intersperse "\n" $ ["MD5Sum:",
+                                          md5s,
+                                          "SHA1Sum:",
+                                          sha1s,
+                                          "SHA256Sum:",
+                                          sha256s]
+  let summary = concat $ intersperse "\n" ["Origin: " ++ origin,
+                                           "Label: " ++ label,
+                                           "Suite: " ++ release,
+                                           "Codename: " ++ release,
+                                           "Components: " ++ component,
+                                           "Architectures: " ++ archs]
+  let payload = pack $ summary ++ "\n" ++ sums ++ "\n"
+  let path = concat $ intersperse "/" [(configDistDir config),
+                                       (configRelease config),
+                                       "Release"]
+
+  (keys, (time, rng)) <- get_key (configKeyPath config)
+  let pgp = sign_msg keys time rng payload
+  B.writeFile (path ++ ".gpg") pgp
+  B.writeFile path payload
+
+
+releaseMkDir distdir release component arch =
+  createDirectoryIfMissing True $ concat $ intersperse "/" [distdir,
+                                                            release,
+                                                            component,
+                                                            "binary-" ++ arch]
+releaseMkDirs ApotikiConfig {distDir = distdir,
+                             architectures = archs,
+                             release = release,
+                             component = component} = do
+  mapM_ (releaseMkDir distdir release component) archs
+
+writeRelease :: ApotikiConfig -> Release -> IO ()
+writeRelease config release = do
+  releaseMkDirs config
+  let pkgs = map (getPkg config) (M.assocs release)
+  release_files <- mapM (writeArchRelease config) (M.assocs release)
+  putStrLn $ "wrote release files: " ++ (show $ length release_files)
+  pkg_files <- mapM (writePackages config) pkgs
+  putStrLn $ "wrote package files: " ++ (show $ length pkg_files)
+  pkg_gz_files <- mapM (writeGzPackages config) pkgs
+  putStrLn $ "wrote package compressed files: " ++ (show $ length pkg_gz_files)
+  writeGlobalRelease config $ concat [release_files, pkg_files, pkg_gz_files]
